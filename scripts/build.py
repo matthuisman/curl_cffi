fb88a754cd9f792a0d9ba2ef76b649f6b804163b
matthuisman <gitlab@matthuisman.nz>
2025-01-31 22:54:03 +1300

    .

--- scripts/build.py
+++ scripts/build.py
@@ -125,6 +125,7 @@ ffibuilder.set_source(
     ],
     extra_compile_args=(["-Wno-implicit-function-declaration"] if system == "Darwin" else []),
     extra_link_args = [
+        '/NODEFAULTLIB:python3.lib',
         "/DEFAULTLIB:python3.8.lib",
         "/VERBOSE"
     ],
